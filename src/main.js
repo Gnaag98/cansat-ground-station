9e31c0e76b3e54fbb5f37251c50e948ed723482f
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2023-05-21 23:37:20 +0200

    Add Chart.js library
    Graphs can now be implemented. Currently random data is being
    written to a line graph.
    
    The timestamp is formatted as 'T+mm:ss'. The
    data is also sorted each iteration in case data is received out of
    order.

--- src/main.js
+++ src/main.js
@@ -1,3 +1,59 @@
+let data = [];
+
+const ctx = document.getElementById('myChart');
+
+chart = new Chart(ctx, {
+    type: 'line',
+    data: {
+        labels: data.map(row => row.x),
+        datasets: [{
+            label: 'Random data',
+            data: data.map(row => row.y),
+            borderWidth: 1
+        }]
+    },
+    options: {
+        animation: true,
+        pointStyle: false,
+        maintainAspectRatio: false,
+        scales: {
+            x: {
+                ticks: {
+                    maxTicksLimit: 11,
+                    callback: (label) => {
+                        let seconds = label % 60;
+                        let minutes = Math.floor(label / 60);
+                        seconds = (seconds < 10 ? '0' : '') + seconds;
+                        minutes = (minutes < 10 ? '0' : '') + minutes;
+                        return `T+${minutes}:${seconds}`;
+                    }
+                }
+            }
+        },
+        plugins: {
+            legend: {
+                display: false
+            }
+        }
+    }
+});
+
+setInterval(() => {
+    const x = data.length;
+    const y = Math.random();
+
+    data.push({ x: x, y: y });
+    // Sort the data in case the data was received out of order.
+    data.sort((a, b) => { return a.x - b.x });
+
+    chart.data.labels = data.map(row => row.x);
+    chart.data.datasets[0].data = data.map(row => row.y);
+    chart.update();
+
+}, 300);
+
+
+
 const socket = new WebSocket("ws://localhost:8765");
 
 let intervalId;
