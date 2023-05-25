11d2efdf829907fa51fc71d0c97c134e25773050
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2023-05-25 12:50:04 +0200

    Convert missing data to None
    Some data can be missing due to errors. These are represented as -1 or
    255 depending on the sensor. These are replaced with None to make it
    clear that this data is missing.

--- src/data.py
+++ src/data.py
@@ -24,6 +24,25 @@ class Data:
     humidity_outside: int
 
 
+# Missing data sent as -1 will be changed to None.
+# Not all sensors return errors.
+def convertNegativeToNone(number: int | float) -> int | float | None:
+    if number < 0:
+        return None
+    else:
+        return number
+
+
+# Missing data sent as 255 will be changed to None.
+# Not all sensors return errors.
+def convert255ToNone(number: int) -> int | None:
+    if number == 255:
+        return None
+    else:
+        return number
+
+
+
 # The CanSat saves space by rounding off floats to three decimals and storing
 # the result as an int. It does this by multiplying the floats by 1000.
 def convertIntToFloat(number: int):
@@ -31,19 +50,29 @@ def convertIntToFloat(number: int):
 
 
 def convertAcceleration(x: int, y: int, z: int):
-    return Acceleration(
-        x=convertIntToFloat(x),
-        y=convertIntToFloat(y),
-        z=convertIntToFloat(z)
-    )
+    x = convertIntToFloat(x)
+    y = convertIntToFloat(y)
+    z = convertIntToFloat(z)
+
+    if x and y and z:
+        return Acceleration(x, y, z)
+    else:
+        return None
 
 
 def deserialize(serialized: bytes):
     deserialized = unpack('<hhhLhhhhBBB', serialized)
 
-    acceleration = convertAcceleration(*deserialized[:3])
-    time = deserialized[3]
-    temperature_outside = convertIntToFloat(deserialized[4])
-    remaining_variables = deserialized[5:]
+    data = Data(
+        acceleration = convertAcceleration(*deserialized[:3]),
+        time = deserialized[3],
+        temperature_outside = convertNegativeToNone(convertIntToFloat(deserialized[4])),
+        sound = deserialized[5],
+        distance = convertNegativeToNone(deserialized[6]),
+        air_quality = deserialized[7],
+        temperature_inside = convert255ToNone(deserialized[8]),
+        humidity_inside = convert255ToNone(deserialized[9]),
+        humidity_outside = convert255ToNone(deserialized[10])
+    )
 
-    return Data(acceleration, time, temperature_outside, *remaining_variables)
+    return data
