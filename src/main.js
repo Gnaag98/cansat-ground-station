2211cd11b21c130d284163fcfecec221201ea62c
Henrik Wahlström <henrik.g.wahlstrom@gmail.com>
2023-05-27 00:13:56 +0200

    Present received data in graph
    The data received by the GUI is accumulated and one of the measurements
    plotted in the graph.
    
    Also fixed the tickmarks so that they show the correct timestamps.

--- src/main.js
+++ src/main.js
@@ -1,14 +1,25 @@
-let data = [];
+let measurements = {
+    acceleration: [],
+    temperature_outside: [],
+    sound: [],
+    distance: [],
+    air_quality: [],
+    temperature_inside: [],
+    humidity_inside: [],
+    humidity_outside: []
+};
+
+let visible_data = 'sound';
 
 const ctx = document.getElementById('myChart');
 
 chart = new Chart(ctx, {
     type: 'line',
     data: {
-        labels: data.map(row => row.x),
+        labels: measurements[visible_data].map(row => row.time),
         datasets: [{
             label: 'Random data',
-            data: data.map(row => row.y),
+            data: measurements[visible_data].map(row => row.data),
             borderWidth: 1
         }]
     },
@@ -19,12 +30,15 @@ chart = new Chart(ctx, {
         scales: {
             x: {
                 ticks: {
+                    
+                    display: true,
                     maxTicksLimit: 11,
-                    callback: (label) => {
-                        let seconds = label % 60;
-                        let minutes = Math.floor(label / 60);
-                        seconds = (seconds < 10 ? '0' : '') + seconds;
-                        minutes = (minutes < 10 ? '0' : '') + minutes;
+                    callback: function(label) {
+                        label = this.getLabelForValue(label);
+                        let minutes = Math.floor(label / 1000 / 60);
+                        let seconds = Math.floor(label / 1000 % 60);
+                        minutes = minutes.toString().padStart(2, '0');
+                        seconds = seconds.toString().padStart(2, '0');
                         return `T+${minutes}:${seconds}`;
                     }
                 }
@@ -51,6 +65,81 @@ function updateChart() {
     chart.update();
 }
 
+function storeData(data) {
+    const time = data['time'];
+    const acceleration = data['acceleration'];
+    const temperature_outside = data['temperature_outside'];
+    const temperature_inside = data['temperature_inside'];
+    const sound = data['sound'];
+    const distance = data['distance'];
+    const air_quality = data['air_quality'];
+    const humidity_inside = data['humidity_inside'];
+    const humidity_outside = data['humidity_outside'];
+    
+    if (acceleration) {
+        measurements.acceleration.push({
+            time: time,
+            x: acceleration['x'],
+            y: acceleration['y'],
+            z: acceleration['z']
+        });
+    }
+
+    if (temperature_outside) {
+        measurements.temperature_outside.push({
+            time: time,
+            data: temperature_outside
+        });
+    }
+
+    if (sound) {
+        measurements.sound.push({
+            time: time,
+            data: sound
+        });
+    }
+
+    if (distance) {
+        measurements.distance.push({
+            time: time,
+            data: distance
+        });
+    }
+
+    if (air_quality) {
+        measurements.air_quality.push({
+            time: time,
+            data: air_quality
+        });
+    }
+
+    if (temperature_inside) {
+        measurements.temperature_inside.push({
+            time: time,
+            data: temperature_inside
+        });
+    }
+
+    if (humidity_inside) {
+        measurements.humidity_inside.push({
+            time: time,
+            data: humidity_inside
+        });
+    }
+
+    if (humidity_outside) {
+        measurements.humidity_outside.push({
+            time: time,
+            data: humidity_outside
+        });
+    }
+
+    // Sort the data in case the data was received out of order.
+    for (const measurement_type in measurements) {
+        measurements[measurement_type].sort((a, b) => { return a.time - b.time });
+    }
+}
+
 const socket = new WebSocket("ws://localhost:8765");
 
 let intervalId;
@@ -64,6 +153,13 @@ socket.onopen = _ => {
 
 socket.onmessage = event => {
     console.log(event.data);
+
+    const received_data = JSON.parse(event.data);
+    storeData(received_data);
+
+    chart.data.labels = measurements[visible_data].map(row => row.time);
+    chart.data.datasets[0].data = measurements[visible_data].map(row => row.data);
+    chart.update();
 };
 
 socket.onclose = _ => {
@@ -78,13 +174,13 @@ startStopButton.addEventListener('click', event => {
         event.target.innerText = 'Stop';
         socket.send('Start');
         console.log('Start');
-        chartIntervalId = setInterval(updateChart, 300);
+        //chartIntervalId = setInterval(updateChart, 300);
     } else {
         if (confirm('Do you really want to stop?')) {
             event.target.innerText = 'Start';
             socket.send('Stop');
             console.log('Stop');
-            clearInterval(chartIntervalId);
+            //clearInterval(chartIntervalId);
             removeEventListener("beforeunload", warnBeforeExiting);
         }
     }
